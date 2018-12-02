5d15f5fbd1561826bdf8c5b203f6b5e8436e2b74
Steph Ng <recurza@gmail.com>
2018-12-01 19:05:49 -0800

    Front page

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="index.css">
+    </head>
+    <body>
+        <?php include("navbar.php") ?>
+        <div class="landing container-fluid">
+            <h1 class="text-center">Be your own DJ, anywhere.</h1>
+            <div class="row justify-content-center m-0">
+                <button type="button" class="btn btn-warning btn-lg col-md-3 m-3">Make a playlist</button>
+                <button type="button" class="btn btn-warning btn-lg col-md-3 m-3">Join a playlist</button>
+            </div>
+        </div>
+        <div class="about container-fluid">
+            <div class="about-content row">
+                <div class="col-md-6">
+                    <h2>Who we are</h2>
+                    <p>
+                    WeJam is a web app that allows you to create collaborative playlists.
+                    <br>
+                    Don’t have a preference? Check out or pre-selected playlists for any music mood.
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    <img class="w-100 align-bottom" src="assets/girl1.png"/>
+                </div>
+            </div>
+        </div>
+        <div class="audience container-fluid">
+            <div class="audience-content row">
+                <div class="col-md-6 order-last order-md-first">
+                    <img class="w-100 align-bottom" src="assets/guy2.png"/>
+                </div>
+                <div class="col-md-6">
+                    <h2>Our Audience</h2>
+                    <p>
+                    People who subscribe to music apps
+                    <br>
+                    Pandora, Spotify, Apple Music,
+                    <br>
+                    Tidal, or Youtube Red
+                    <br>
+                    People who share music
+                    <br>
+                    Restaurants who want to give their customers the ability to control what music they listen to 
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,31 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light container">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand nav-justified" href="#">Hidden brand</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="top-songs.php"><li>Top Songs</li></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="about.php"><li>About</li></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="team.php"><li>Our Team</li></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="contact.php"><li>Contact</li></a>
+      </li>
+    </ul>
+
+    <ul class="navbar-nav my-2 my-lg-0">
+      <li class="nav-item">
+        <a class="nav-link" href="accountcreation.php"><li>Sign Up</li></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="login.php"><li>Login</li></a>
+      </li>
+</ul>
+  </div>
+</nav>
